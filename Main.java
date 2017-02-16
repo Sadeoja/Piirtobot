7715efbf2bf0571fcd931c110419c8aa24bebb84
JimiJokela1 <jimi.jokela@metropolia.fi>
2017-02-16 12:27:22 +0200

    more testing basic movement

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
@@ -12,23 +13,38 @@ public class Main {
 	static RegulatedMotor lifterMotor = new EV3MediumRegulatedMotor(MotorPort.B);
 	
 	public static void main(String[] args) {
-		
-		rightMotor.setSpeed(1000);
-		leftMotor.setSpeed(1000);
-		lifterMotor.setSpeed(1000);
+		String valinta = "vapaa";
+		boolean lcdRefresh = false;
+		rightMotor.setSpeed(300);
+		leftMotor.setSpeed(300);
+		lifterMotor.setSpeed(300);
 		// forward -> käsi liikkuu oikealle
 		// backward -> käsi liikkuu vasemmalle
-		
+		LCD.drawString(valinta, 0, 0);
 		while (true){
-			if (Button.DOWN.isDown()){
-				drawLine(0);
-			}else if (Button.UP.isDown()){
-				drawLine(1);
-			}else if (Button.RIGHT.isDown()){
-				drawLine(2);
-			}else if (Button.LEFT.isDown()){
-				drawLine(3);
-			}else if (Button.ESCAPE.isDown()){
+			if (valinta == "vapaa"){
+				if (Button.DOWN.isDown()){
+					drawLine("eteen");
+				}else if (Button.UP.isDown()){
+					drawLine("taakse");
+				}else if (Button.RIGHT.isDown()){
+					drawLine("vasen");
+				}else if (Button.LEFT.isDown()){
+					drawLine("oikea");
+				} else if (Button.ENTER.isDown()){
+					nelio(15);
+				}
+//			} else if (valinta == "nelio"){
+//				if (Button.ENTER.isDown()){
+//					nelio(100);
+//				}
+//			} else if (valinta == "kolmio"){
+//				if (Button.ENTER.isDown()){
+//					kolmio(100);
+//				}
+			}
+			
+			if (Button.ESCAPE.isDown()){
 				break;
 			}
 		}
@@ -40,26 +56,103 @@ public class Main {
 		lifterMotor.close();
 	}
 	
-	static void drawLine(int dir){
+	static void drawLine(String dir){
 		rightMotor.synchronizeWith(new RegulatedMotor[] {leftMotor});
 		rightMotor.startSynchronization();
 		leftMotor.startSynchronization();
-		if (dir == 0){
-			rightMotor.rotate(360);
-			leftMotor.rotate(-360);
-		} else if (dir == 1){
+		if (dir == "eteen"){
 			rightMotor.rotate(-360);
 			leftMotor.rotate(360);
-		} else if (dir == 2){
+		} else if (dir == "taakse"){
+			rightMotor.rotate(360);
+			leftMotor.rotate(-360);
+		} else if (dir == "oikea"){
 			rightMotor.rotate(360);
 			leftMotor.rotate(360);
-		} else if (dir == 3){
+		} else if (dir == "vasen"){
 			rightMotor.rotate(-360);
 			leftMotor.rotate(-360);
+		} else if (dir == "takavasen"){
+			leftMotor.rotate(-1000);
 		}
 		
 		rightMotor.endSynchronization();
 		leftMotor.endSynchronization();
+	}
+	
+	static void kolmio(int size){
+		for (int i = 0; i < size; i++){
+			drawLine("oikea");
+		}
+		Delay.msDelay(100);
+		for (int i = 0; i < size; i++){
+			drawLine("eteen");		
+		}
+		Delay.msDelay(100);
+		for (int i = 0; i < size * 1.4; i++){
+			drawLine("takavasen");
+		}
+		Delay.msDelay(100);
+	}
+	
+	static void nelio(int size){
+//		for (int i = 0; i < size; i++){
+//			drawLine("oikea");
+//		}
+//		Delay.msDelay(500);
+//		for (int i = 0; i < size; i++){
+//			drawLine("eteen");
+//		}
+//		Delay.msDelay(500);
+//		for (int i = 0; i < size; i++){
+//			drawLine("vasen");
+//		}
+//		Delay.msDelay(500);
+//		for (int i = 0; i < size; i++){
+//			drawLine("taakse");
+//		}
+//		Delay.msDelay(500);
+		
+		drawLine("oikea");
 		Delay.msDelay(500);
+		drawLine("eteen");
+		Delay.msDelay(500);
+		drawLine("vasen");
+		Delay.msDelay(500);
+		drawLine("taakse");
+		Delay.msDelay(500);
+	}
+	
+	static void drawLineXY(int x, int y){
+		rightMotor.synchronizeWith(new RegulatedMotor[] {leftMotor});
+		rightMotor.startSynchronization();
+		leftMotor.startSynchronization();
+		int factor = 1;
+		if (x > 0 && y > 0){
+			rightMotor.rotate(x * factor);
+			leftMotor.rotate(y * factor);
+		} else if (x == 0 && y > 0){
+			rightMotor.rotate(-y * factor);
+			leftMotor.rotate(y * factor);
+		} else if (x > 0 && y == 0){
+			rightMotor.rotate(x * factor);
+			leftMotor.rotate(x * factor);
+		} else if (x < 0 && y > 0){
+			leftMotor.rotate(x * y * factor);
+		} else if (x < 0 && y == 0){
+			rightMotor.rotate(x * factor);
+			leftMotor.rotate(x * factor);
+		} else if (x > 0 && y < 0){
+			rightMotor.rotate(x * y * factor);
+		} else if (x == 0 && y < 0){
+			rightMotor.rotate(-y * factor);
+			leftMotor.rotate(y * factor);
+		} else if (x < 0 && y < 0){
+			rightMotor.rotate(x * factor);
+			leftMotor.rotate(y * factor);
+		}
+		
+		rightMotor.endSynchronization();
+		leftMotor.endSynchronization();
 	}
 }
